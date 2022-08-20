4a69ac1f88a02c982efcb7c1b946dff5b6d1b31c
Mr.chu <chuhongkyu@gmail.com>
2022-08-20 19:00:42 +0900

    넥스트 환경설정

--- index.js
+++
@@ -1,9 +0,0 @@
-const Loading = document.querySelector(".box_containers");
-const HIDDEN_CLASS = "hidden";
-
-function disappear() {
-    Loading.classList.add(HIDDEN_CLASS);
-  }
-
-
-  setTimeout(disappear, 2500);
