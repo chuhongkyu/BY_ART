fce3ecb582ed55cbe2be081475a71a5fb8d16534
Mr.chu <chuhongkyu@gmail.com>
2022-08-25 09:33:23 +0900

    [Next] Link,Router

---
+++ components/NavBar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <nav>
+      <Link href="/">
+        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+          About
+        </a>
+      </Link>
+    </nav>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
-import moment from "moment";
-import type { NextPage } from "next";
 import { useState } from "react";
+import NavBar from "../components/NavBar";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
-  const [getMoment, setMoment] = useState(moment()); // 오늘
-
-  const today = getMoment; // 오늘
-
-  const todayFirstWeek = today.clone().startOf("month").week(); // 이번달의 첫째 주
-  const todayLastWeek =
-    today.clone().endOf("month").week() === 1
-      ? 53
-      : today.clone().endOf("month").week(); // 이번달의 마지막 주
-
-  const [dayArray, setDayArray] = useState<any>([]); // 날짜들이 가지는 상태값들 모아둔 배열
-
-  return <div className={styles.container}></div>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Home</h1>
+    </div>
+  );
+}
