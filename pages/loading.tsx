568d22d6ce29544918b59ad6e05a9f9b83193ba9
Mr.chu <chuhongkyu@gmail.com>
2022-08-25 14:17:20 +0900

    로딩 페이지

--- pages/loading.tsx
+++ pages/loading.tsx
@@ -1,16 +1,35 @@
-class Artwork {
-  name: string;
-  date: number;
-  description: string;
-  constructor(name: string, date: number, description: string) {
-    this.name = name;
-    this.date = date;
-    this.description = description;
-  }
-}
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
 
-const picture = new Artwork(
-  "BY_Man_2",
-  20211206,
-  "This picture is a pixel picture drawn in blue and yellow. It is the Artwork of Mr.chu."
-);
+const loading = () => {
+  const router = useRouter();
+  const onLocate = () => {
+    router.push("/");
+  };
+  useEffect(() => {
+    setTimeout(onLocate, 1500);
+    return () => {
+      clearTimeout();
+    };
+  }, []);
+  return (
+    <section className={styles.loading}>
+      <div className={styles.box_containers}>
+        <div className={styles.box_container}>
+          <div className={styles.box_container_1}>
+            <div className={styles.box1}></div>
+            <div className={styles.box2}></div>
+          </div>
+
+          <div className={styles.box_container_2}>
+            <div className={styles.box3}></div>
+            <div className={styles.box4}></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default loading;
